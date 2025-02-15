f5893d371bc3621c357e72d07de4b41a89d46ffd
Caceresenzo <caceresenzo1502@gmail.com>
2025-02-15 23:15:40 +0100

    release: bump to 0.2.1

--- with_env/__version__.py
+++ with_env/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'with-env'
 __description__ = 'with-env - Start a process with the nearby .env!'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __author__ = 'Enzo CACERES'
 __author_email__ = 'caceresenzo1502@gmail.com'
 __url__ = 'https://github.com/Caceresenzo/with-env'
