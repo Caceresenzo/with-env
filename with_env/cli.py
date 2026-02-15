4a47f600c31424d9d61cba5e690258f959cff9a3
Caceresenzo <caceresenzo1502@gmail.com>
2026-02-15 01:08:05 +0100

    feat(cli): add `--file` option to specify env file paths

--- with_env/cli.py
+++ with_env/cli.py
@@ -1,27 +1,48 @@
-import click
 import sys
+from typing import List, Sequence
+
+import click
 
 from .execute import ProgramExecutor, RestartableProgramExecutor
 from .watch import FileObserver
 
 
+def _remove_duplicates(items: Sequence[str]) -> List[str]:
+    seen = set()
+    uniques = []
+
+    for item in items:
+        if item not in seen:
+            seen.add(item)
+            uniques.append(item)
+        else:
+            print(f"with-env: {item}: specified multiple times", file=sys.stderr)
+
+    return uniques
+
+
 @click.command(context_settings={"allow_interspersed_args": False})
 @click.option("--watch", "-w", "watch_for_changes", is_flag=True, help="Watch for changes in the .env and restart the command.")
+@click.option("--file", "-f", "extra_env_files", multiple=True, help="Specify additional .env files to load.")
 @click.argument("argv", nargs=-1)
 def cli(
     watch_for_changes: bool,
+    extra_env_files: list[str],
     argv: list[str],
 ):
-    env_files = set([
+    env_files = [
         ".env",
-    ])
+        *extra_env_files,
+    ]
 
     while len(argv) and argv[0].startswith(":"):
         if isinstance(argv, tuple):
             argv = list(argv)
 
         profile = argv.pop(0)[1:]
-        env_files.add(f".env.{profile}")
+        env_files.append(f".env.{profile}")
+
+    env_files = _remove_duplicates(env_files)
 
     if not len(argv):
         print(f"with-env: no command specified", file=sys.stderr)
